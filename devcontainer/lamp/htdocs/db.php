fca084ad7d16ef4eaaa1182515b7241dd6c50989
tamfoi <tamfoi@users.noreply.github.com>
2020-05-16 00:40:02 +0900

    update:コンテナ間通信で完結するようにした

--- devcontainer/lamp/htdocs/db.php
+++ devcontainer/lamp/htdocs/db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-  $dbh = new PDO('mysql:host=host.docker.internal;dbname=mysql;port=8200', 'root', 'password');
+  $dbh = new PDO('mysql:host=db;dbname=mysql;port=3306', 'root', 'password');
 
   foreach($dbh->query('select * from time_zone_name') as $row) {
     echo $row['Name'] . '<br>';
